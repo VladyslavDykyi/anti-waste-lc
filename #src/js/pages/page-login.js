b65e23f5a73855666c5ca2b6edd549855ba5f78d
Vladyslav Dykyi <vladislavdykyi33@gmail.com>
2022-09-19 18:22:03 +0300

    страницы логина регистрации востановления пароля кода и спасибо страницы

--- #src/js/pages/page-login.js
+++ #src/js/pages/page-login.js
@@ -1,15 +1,18 @@
-const eyePassword = document.querySelector('.eye-through'),
-	eyeClose = eyePassword.querySelector('.eye-close'),
-	eyeOpen = eyePassword.querySelector('.eye-open');
+const eyePassword = document.querySelectorAll('.eye-through');
 
-eyePassword.addEventListener('click', () => {
-	if(eyePassword.previousElementSibling.getAttribute('type') === 'password') {
-		eyePassword.previousElementSibling.setAttribute('type', 'text');
-		eyeClose.classList.remove('active');
-		eyeOpen.classList.add('active');
-	} else {
-		eyePassword.previousElementSibling.setAttribute('type', 'password');
-		eyeClose.classList.add('active');
-		eyeOpen.classList.remove('active');
-	}
-})
+
+for (let i = 0; i < eyePassword.length; i++) {
+	let eyeClose = eyePassword[i].querySelector('.eye-close');
+	let eyeOpen = eyePassword[i].querySelector('.eye-open');
+	eyePassword[i].addEventListener('click', () => {
+		if (eyePassword[i].previousElementSibling.getAttribute('type') === 'password') {
+			eyePassword[i].previousElementSibling.setAttribute('type', 'text');
+			eyeClose.classList.remove('active');
+			eyeOpen.classList.add('active');
+		} else {
+			eyePassword[i].previousElementSibling.setAttribute('type', 'password');
+			eyeClose.classList.add('active');
+			eyeOpen.classList.remove('active');
+		}
+	})
+}
