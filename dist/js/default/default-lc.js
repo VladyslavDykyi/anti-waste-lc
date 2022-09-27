0473647a6a983817cc7df65ad31fe1e6e1fa3a19
Vladyslav Dykyi <vladislavdykyi33@gmail.com>
2022-09-28 01:02:26 +0300

    pages add product js

--- dist/js/default/default-lc.js
+++ dist/js/default/default-lc.js
@@ -15,10 +15,14 @@ fileFields('.file-inp');
 function fileFields(inp) {
 	const avatar = document.querySelectorAll(inp);
 	for (let i = 0; i < avatar.length; i++) {
-		avatar[i].addEventListener('input', () => {
-			avatar[i].parentElement.nextElementSibling.firstElementChild.setAttribute('src', URL.createObjectURL(avatar.files[0]));
-			avatar[i].parentElement.nextElementSibling.lastElementChild.previousElementSibling.srcset = URL.createObjectURL(avatar.files[0]);
-			avatar[i].parentElement.nextElementSibling.firstElementChild.classList.add('active');
+		avatar[i].addEventListener('input', function() {
+			let reader = new FileReader();
+			reader.onload = function (e) {
+				avatar[i].parentElement.nextElementSibling.lastElementChild.setAttribute('src', e.target.result);
+				avatar[i].parentElement.nextElementSibling.firstElementChild.srcset = e.target.result;
+			}
+			reader.readAsDataURL(this.files[0]);
+			avatar[i].parentElement.nextElementSibling.lastElementChild.classList.add('active');
 			avatar[i].parentElement.classList.add('none');
 			avatar[i].parentElement.parentElement.classList.add('active');
 			deleteFile(avatar[i].parentElement.nextElementSibling.nextElementSibling.firstElementChild);
