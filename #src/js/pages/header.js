150c4ac45f7ae48999b53f31eb508bc979f83ebe
VladyslavDykyi <vladislavdykyi33@gmail.com>
2022-11-01 23:49:29 +0200

    хедер меню

--- #src/js/pages/header.js
+++ #src/js/pages/header.js
@@ -7,6 +7,9 @@ const arrSvg = [
 		<path fill-rule="evenodd" clip-rule="evenodd" d="M10 16.7692C10.8889 16.7692 11.7692 16.5941 12.5905 16.254C13.4118 15.9138 14.158 15.4152 14.7866 14.7866C15.4152 14.158 15.9138 13.4118 16.254 12.5905C16.5941 11.7692 16.7692 10.8889 16.7692 10C16.7692 9.11105 16.5941 8.23081 16.254 7.40953C15.9138 6.58825 15.4152 5.84201 14.7866 5.21343C14.158 4.58485 13.4118 4.08623 12.5905 3.74605C11.7692 3.40586 10.8889 3.23077 10 3.23077C8.20469 3.23077 6.48291 3.94395 5.21343 5.21343C3.94395 6.48291 3.23077 8.20469 3.23077 10C3.23077 11.7953 3.94395 13.5171 5.21343 14.7866C6.48291 16.056 8.20469 16.7692 10 16.7692ZM18 10C18 12.1217 17.1571 14.1566 15.6569 15.6569C14.1566 17.1571 12.1217 18 10 18C7.87827 18 5.84344 17.1571 4.34315 15.6569C2.84285 14.1566 2 12.1217 2 10C2 7.87827 2.84285 5.84344 4.34315 4.34315C5.84344 2.84285 7.87827 2 10 2C12.1217 2 14.1566 2.84285 15.6569 4.34315C17.1571 5.84344 18 7.87827 18 10Z" fill="#1D350A"/>
 		<path d="M14 15.9784C14.0424 16.0349 14.0877 16.0887 14.1386 16.141L19.5845 21.5854C19.8497 21.8507 20.2095 21.9999 20.5847 22C20.9599 22.0001 21.3198 21.8512 21.5853 21.5861C21.8507 21.3209 21.9999 20.9612 22 20.5861C22.0001 20.211 21.8512 19.8511 21.586 19.5858L16.1401 14.1414C16.0896 14.0902 16.0352 14.043 15.9775 14C15.4226 14.7564 14.7558 15.424 14 15.9798V15.9784Z" fill="#1D350A"/>
 	</svg>`,
+	`<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<path fill-rule="evenodd" clip-rule="evenodd" d="M2 19.1111C2 18.8754 2.09099 18.6493 2.25295 18.4826C2.41492 18.3159 2.63459 18.2222 2.86364 18.2222H20.1364C20.3654 18.2222 20.5851 18.3159 20.747 18.4826C20.909 18.6493 21 18.8754 21 19.1111C21 19.3469 20.909 19.573 20.747 19.7397C20.5851 19.9064 20.3654 20 20.1364 20H2.86364C2.63459 20 2.41492 19.9064 2.25295 19.7397C2.09099 19.573 2 19.3469 2 19.1111ZM2 12C2 11.7643 2.09099 11.5382 2.25295 11.3715C2.41492 11.2048 2.63459 11.1111 2.86364 11.1111H20.1364C20.3654 11.1111 20.5851 11.2048 20.747 11.3715C20.909 11.5382 21 11.7643 21 12C21 12.2357 20.909 12.4618 20.747 12.6285C20.5851 12.7952 20.3654 12.8889 20.1364 12.8889H2.86364C2.63459 12.8889 2.41492 12.7952 2.25295 12.6285C2.09099 12.4618 2 12.2357 2 12ZM2 4.88889C2 4.65314 2.09099 4.42705 2.25295 4.26035C2.41492 4.09365 2.63459 4 2.86364 4H20.1364C20.3654 4 20.5851 4.09365 20.747 4.26035C20.909 4.42705 21 4.65314 21 4.88889C21 5.12464 20.909 5.35073 20.747 5.51743C20.5851 5.68413 20.3654 5.77778 20.1364 5.77778H2.86364C2.63459 5.77778 2.41492 5.68413 2.25295 5.51743C2.09099 5.35073 2 5.12464 2 4.88889Z" fill="#1D350A"/>
+	</svg>`,
 ];
 
 const btn = document.querySelector('#btn-open-search');
@@ -25,6 +28,7 @@ btn.addEventListener('click', e => {
 	e.stopPropagation();
 	toggleSearch();
 });
+
 document.addEventListener('click', e => {
 	let target = e.target;
 	let its_menu = target === searchBox || searchBox.contains(target);
@@ -39,11 +43,13 @@ document.addEventListener('click', e => {
 const langCont = document.querySelector('.header-lang ');
 const btnLang = document.querySelector('.header-lang > button');
 const dropDown = document.querySelector('.header-lang > .drop-down');
+
 btnLang.addEventListener('click', e => {
 	e.stopPropagation();
 	langCont.classList.toggle('active');
 	dropDown.classList.toggle('active');
 });
+
 document.addEventListener('click', e => {
 	let target = e.target;
 	let its_menu = target === dropDown || dropDown.contains(target);
@@ -52,5 +58,37 @@ document.addEventListener('click', e => {
 
 	if (!its_menu && !its_btn && menu_is_active) {
 		langCont.classList.toggle('active');
+	}
+});
+
+const burger = document.querySelector('#burger');
+const header_mob = document.querySelector('.header-mob');
+const body = document.querySelector('body');
+const header_mob_menu = document.querySelector('.header-mob-menu');
+
+const toggleBurger = () => {
+	burger.classList.toggle('active');
+	header_mob.classList.toggle('active');
+	body.classList.toggle('lock');
+	if (burger.classList.contains('active')) {
+		burger.innerHTML = arrSvg[0];
+	} else {
+		burger.innerHTML = arrSvg[2];
+	}
+}
+
+burger.addEventListener('click', e => {
+	e.stopPropagation();
+	toggleBurger();
+});
+
+document.addEventListener('click', e => {
+	let target = e.target;
+	let its_menu = target === header_mob_menu || header_mob_menu.contains(target);
+	let its_btn = target === burger;
+	let menu_is_active = header_mob.classList.contains('active');
+
+	if (!its_menu && !its_btn && menu_is_active) {
+		toggleBurger();
 	}
 });
