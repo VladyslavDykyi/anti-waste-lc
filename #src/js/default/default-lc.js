22c95ba32f121caf45991f949f750e79f7bf246e
VladyslavDykyi <vladislavdykyi33@gmail.com>
2022-12-06 21:36:58 +0200

    upd request end

--- #src/js/default/default-lc.js
+++ #src/js/default/default-lc.js
@@ -1,4 +1,19 @@
-$(document).ready(function() {
+
+
+$(document).ready(function () {
+	// updBasket();
+
+	const today = new Date();
+	const yyyy = today.getFullYear();
+	let mm = today.getMonth() + 1; // Months start at 0!
+	let dd = today.getDate();
+
+	if (dd < 10) dd = '0' + dd;
+	if (mm < 10) mm = '0' + mm;
+
+	const formattedToday = yyyy + '-' + mm + '-' + dd;
+
+
 	$('.js-example-basic-single').select2({
 		minimumResultsForSearch: -1,
 		theme: "my",
@@ -6,6 +21,23 @@ $(document).ready(function() {
 	$('#modal1').on('click', (e) => {
 		if (!e.target.parentElement.parentElement.classList.contains('successfully')) {
 			$('#exampleModal1').arcticmodal();
+			$.ajaxSetup({
+				headers: {
+					'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+				}
+			});
+
+			$.ajax({
+				type: 'post',
+				url: '/email/verification-notification',
+				contentType: false,
+				cache: false,
+				processData: false,
+				success: function (result) {
+					document.querySelector('.arcticmodal-container').querySelector('.arcticmodal-close').click()
+					$('#exampleModal2').arcticmodal();
+				}
+			});
 		} else {
 			return;
 		}
@@ -25,11 +57,27 @@ $(document).ready(function() {
 			return;
 		}
 	});
-	$('.date').daterangepicker({
+	$('#final_expiration_date').daterangepicker({
 		"singleDatePicker": true,
-		"startDate": "09/21/2022",
-		"endDate": "09/27/2022"
-	}, function(start, end, label) {});
+		"startDate": $('#final_expiration_date').val() ? $('#final_expiration_date').val() : formattedToday,
+		"endDate": $('#final_expiration_date').val() ? $('#final_expiration_date').val() : formattedToday,
+		'locale': {
+			format: 'YYYY-MM-DD'
+		}
+	}, function (start, end, label) { });
+
+	$('#ad_is_active_until').daterangepicker({
+		"singleDatePicker": true,
+		"startDate": $('#ad_is_active_until').val() ? $('#ad_is_active_until').val() : formattedToday,
+		"endDate": $('#ad_is_active_until').val() ? $('#ad_is_active_until').val() : formattedToday,
+		'locale': {
+			format: 'YYYY-MM-DD'
+		}
+	}, function (start, end, label) { });
+
+	$('.date').on('apply.daterangepicker', function (ev, picker) {
+		$(this).attr('value', picker.startDate.format('YYYY-MM-DD'));
+	});
 });
 
 
